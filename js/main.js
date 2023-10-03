b1f5c9a67b5a8a760a52e75205573cf1d8dee091
Muhammad Xamidov <muhammadxamidov001@gmail.com>
2023-10-03 19:46:11 +0300

    Add action to menubar of site-header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const modifiers = {
+    siteHeaderActive: "site-header_active",
+    sitenavTogglerActive: "site-header__sitenav-toggler_actice"
+};
+
+const elSiteHeader = document.querySelector('.site-header');
+const elSitenavToggler = elSiteHeader.querySelector('.site-header__sitenav-toggler');
+
+if (elSitenavToggler) {
+    elSitenavToggler.addEventListener("click", function () {
+        elSitenavToggler.classList.toggle(modifiers.sitenavTogglerActive);
+        elSiteHeader.classList.toggle(modifiers.siteHeaderActive);
+    });
+};
